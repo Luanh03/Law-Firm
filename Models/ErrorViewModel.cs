9e04ee89de0ff866883ac1647f71a56de07188c5
Luan Henrique <luanhcosta03@gmail.com>
2025-05-23 23:13:07 -0300

    Basic development structure

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Law_Firm.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
