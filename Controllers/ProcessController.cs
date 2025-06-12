522fa69f631fa93f3f05224aa0e280f1802ec89a
Luan Henrique <luanhcosta03@gmail.com>
2025-06-11 22:23:30 -0300

    Implementing View Index and Create Section Process

--- Controllers/ProcessController.cs
+++ Controllers/ProcessController.cs
@@ -8,5 +8,10 @@ namespace Law_Firm.Controllers
         {
             return View();
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
     }
 }
--- Models/Process.cs
+++ Models/Process.cs
@@ -2,5 +2,11 @@
 {
     public class Process
     {
+        public int Id { get; set; }
+        public required string TRT { get; set; }
+        public required string CNPJ { get; set; }
+        public required string Rod { get; set; }
+        public required string Processes { get; set; }
+        public required string Year { get; set; }
     }
 }
